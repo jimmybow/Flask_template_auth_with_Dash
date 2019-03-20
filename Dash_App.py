94cea49e8cb9fdb52b0d5b314a431b8c0f091bba
jimmy <jimmywu@bluetechnology.com.tw>
2019-03-20 17:30:48 +0800

    wr

--- Dash_App.py
+++ Dash_App.py
@@ -8,6 +8,7 @@ from dash import Dash
 from dash.dependencies import Input, State, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from urllib.parse import parse_qs
 import pandas as pd
 from datetime import datetime, timedelta
 from Dash_fun import serve_layout, load_object, save_object
@@ -41,17 +42,15 @@ def Add_Dash(server, create_secret):
     url_base_pathname = '/dash/app1/'
     app = Dash(server=server, url_base_pathname=url_base_pathname)
     app.config.supress_callback_exceptions = True
-    app.layout = app.layout = serve_layout
+    app.layout = serve_layout
 
     @app.callback(
             Output('index', 'children'),
             [Input('url', 'search')])
     def display_page(request_args):
         if request_args:
-            rr = pd.Series(str(request_args)[1:].split('&')).str.split('=')
-            key = rr.str.get(0)
-            value = rr.str.slice(1,).str.join('=')   
-            if 'secret' in list(key) and value[key == 'secret'].iloc[0] == create_secret(url_base_pathname):
+            url_args = parse_qs(request_args[1:])   
+            if 'secret' in url_args and url_args['secret'][0] == create_secret(url_base_pathname):
                 return layout
         return html.Div('Error ! 不可訪問 !')
     
--- Dash_App2.py
+++ Dash_App2.py
@@ -8,6 +8,7 @@ from dash import Dash
 from dash.dependencies import Input, State, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from urllib.parse import parse_qs
 import pandas as pd
 from datetime import datetime, timedelta
 from Dash_fun import serve_layout, load_object, save_object
@@ -25,20 +26,9 @@ def Add_Dash(server, create_secret):
             [Input('url', 'search')])
     def display_page(request_args):
         if request_args:
-            rr = pd.Series(str(request_args)[1:].split('&')).str.split('=')
-            key = rr.str.get(0)
-            value = rr.str.slice(1,).str.join('=')   
-            if 'secret' in list(key) and value[key == 'secret'].iloc[0] == create_secret(url_base_pathname):
+            url_args = parse_qs(request_args[1:])   
+            if 'secret' in url_args and url_args['secret'][0] == create_secret(url_base_pathname):
                 return layout
         return html.Div('Error ! 不可訪問 !')
-    
-    @app.callback(
-            Output('Dropdown', 'options'),
-            [Input('time_start', 'date'),
-             Input('time_end', 'date')])
-    def callback(time_start, time_end):
-        return [{'label': '顯示所有', 'value': 'all'},
-                {'label': 'time_start', 'value': time_start},
-                {'label': 'time_end',   'value': time_end}     ]
     
     return app.server, url_base_pathname
